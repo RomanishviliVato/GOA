4ee8cc94cc3ec28986c952f828264558f64041f1
RomanishviliVato <juzejuzemurinio@gmail.com>
2025-01-28 22:16:38 +0400

    commit

---
+++ level 001/homework/lesson1.py
@@ -0,0 +1,5 @@
+name = "vato"
+surname = "romanishvili"
+age = 15
+
+print(f"Hello, my name is {name} {surname}. I am {age} years old.")
---
+++ level 003/homewrok/homework.py
@@ -0,0 +1,28 @@
+score=15
+print(score)
+
+health=10
+print(health)
+
+atacking_points=20
+defence_points=25
+
+print(atacking_points,defence_points)
+print(" ")
+
+math_score=9
+english_score=8
+science_score=10
+chemistry_score=7
+history_score=6
+
+print(math_score)
+print(english_score)
+print(science_score)
+print(chemistry_score)
+print(history_score)
+
+name="vato"
+surname="romanishvili"
+age=15
+print(f"Hello, my name is {name} {surname}. I am {age} years old.")
